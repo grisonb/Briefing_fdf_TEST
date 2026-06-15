2e736991552b864811936c888a93341ef3a449f7
grisonb <79510016+grisonb@users.noreply.github.com>
2026-06-15 12:36:39 +0200

    Update CACHE_NAME for service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'briefing-fdf-test-v3.3-pwa-tdf2026-tuto-sauvegarde';
+const CACHE_NAME = 'briefing-fdf-test-v3.4-pwa-tdf2026-nouveau-mdp-nats';
 
 const LOCAL_ASSETS = [
   './manifest.json',
