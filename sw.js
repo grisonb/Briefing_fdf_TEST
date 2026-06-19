c31ab07e08e56105354ca2f7c53ce9de06abe244
grisonb <79510016+grisonb@users.noreply.github.com>
2026-06-19 20:50:55 +0200

    Update CACHE_NAME to version 3.27 with timeout

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'briefing-fdf-test-v3.26-notams-no-scroll-horodatage';
+const CACHE_NAME = 'briefing-fdf-test-v3.27-notams-timeout-30s';
 
 const LOCAL_ASSETS = [
   './manifest.json',
