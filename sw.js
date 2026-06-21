debf2ab479a25fb658b4b20c33b0b185c4153459
grisonb <79510016+grisonb@users.noreply.github.com>
2026-06-21 22:50:11 +0200

    Update CACHE_NAME for service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'briefing-fdf-test-v3.49-fds-sans-bouton-nas';
+const CACHE_NAME = 'briefing-fdf-test-v3.50-fds-mode-purge-session';
 
 const LOCAL_ASSETS = [
   './manifest.json',
