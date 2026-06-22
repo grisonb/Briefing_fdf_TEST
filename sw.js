15269e41637d3c85d5ec2ea3d7820f7ac35cbe89
grisonb <79510016+grisonb@users.noreply.github.com>
2026-06-22 21:12:44 +0200

    Update CACHE_NAME to version 3.61

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'briefing-fdf-test-v3.60-sans-restaurer-status-haut';
+const CACHE_NAME = 'briefing-fdf-test-v3.61-sans-restaurer-status-haut';
 
 const LOCAL_ASSETS = [
   './manifest.json',
