248e54a75c6e08e3bfb0c6799bb27cb9fd0b250d
grisonb <79510016+grisonb@users.noreply.github.com>
2026-06-30 09:56:34 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'briefing-fdf-test-v4.06-pencil-notams-suppression-fiable';
+const CACHE_NAME = 'briefing-fdf-test-v4.07-pencil-notams-ligne-verrouillee-scroll-lock';
 
 const LOCAL_ASSETS = [
   './manifest.json',
