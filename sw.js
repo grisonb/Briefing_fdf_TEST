0c0c7908e951e7e728238638ed75e176ce8fed89
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-02 19:55:08 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'briefing-fdf-test-v4.24-gaar-relais-nas-partage-jour';
+const CACHE_NAME = 'briefing-fdf-test-v4.25-bandeau-vert-annotations-pdf';
 
 const LOCAL_ASSETS = [
   './manifest.json',
