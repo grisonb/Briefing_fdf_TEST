858bc8f5d9ff1a5245835dbe207c45abb27f9958
grisonb <79510016+grisonb@users.noreply.github.com>
2026-07-05 13:04:00 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'briefing-fdf-test-v4.28-carte-supaip-foreflight';
+const CACHE_NAME = 'briefing-fdf-test-v4.29-azba-supaip-groupe';
 
 const LOCAL_ASSETS = [
   './manifest.json',
