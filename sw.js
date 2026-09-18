3af6f6704cce9b315c296a317d1a597ca5c73985
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-18 11:20:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const BFG_SW_VERSION = '4.64';
-const CACHE_NAME = 'briefing-fdf-test-v4.64-npf-reassociation-r1';
+const BFG_SW_VERSION = '4.65';
+const CACHE_NAME = 'briefing-fdf-test-v4.65-npf-collapsible-r1';
 
 const LOCAL_ASSETS = [
   './manifest.json',
