4f6ee5f35d815efd7c2fb831a4be170812330715
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-20 14:52:56 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const BFG_SW_VERSION = '4.77';
-const CACHE_NAME = 'briefing-fdf-test-v4.77-notams-dual-weather-r1';
+const BFG_SW_VERSION = '4.78';
+const CACHE_NAME = 'briefing-fdf-test-v4.78-notams-source-separee-grue-r1';
 
 const LOCAL_ASSETS = [
   './manifest.json',
