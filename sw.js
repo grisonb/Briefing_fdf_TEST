8ac4bb4e3efcb8e7b4d5a5196d76a2b25dc61bd3
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-20 10:02:35 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const BFG_SW_VERSION = '4.71';
-const CACHE_NAME = 'briefing-fdf-test-v4.71-supaip-render-fix-r1';
+const BFG_SW_VERSION = '4.72';
+const CACHE_NAME = 'briefing-fdf-test-v4.72-sdvfr-notams-r1';
 
 const LOCAL_ASSETS = [
   './manifest.json',
