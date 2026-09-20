e4694f6d4b19a4250016a77fb2842d1e8fe99a43
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-20 11:06:27 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const BFG_SW_VERSION = '4.74';
-const CACHE_NAME = 'briefing-fdf-test-v4.74-sdvfr-pelic-filter-r1';
+const BFG_SW_VERSION = '4.75';
+const CACHE_NAME = 'briefing-fdf-test-v4.75-sdvfr-weather-compare-r1';
 
 const LOCAL_ASSETS = [
   './manifest.json',
