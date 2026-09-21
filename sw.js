cb614fca8f5792e49a96d9142b11366ed09b063e
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-21 17:31:26 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const BFG_SW_VERSION = '5.14';
-const CACHE_NAME = 'briefing-fdf-test-v5.14-notam-paragraphs-r1';
+const BFG_SW_VERSION = '5.15';
+const CACHE_NAME = 'briefing-fdf-test-v5.15-notam-clear-persistence-r1';
 
 const LOCAL_ASSETS = [
   './manifest.json',
