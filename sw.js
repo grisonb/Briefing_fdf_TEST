959879620b679ddcf08b41f0f4dfa5d0b6f0adfc
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-21 15:58:16 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const BFG_SW_VERSION = '5.08';
-const CACHE_NAME = 'briefing-fdf-test-v5.08-supaip-large-map-r1';
+const BFG_SW_VERSION = '5.09';
+const CACHE_NAME = 'briefing-fdf-test-v5.09-notam-weather-r1';
 
 const LOCAL_ASSETS = [
   './manifest.json',
