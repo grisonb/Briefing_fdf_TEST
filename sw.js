0357ab151c15711d7bee1f139c7df72d201375ff
grisonb <79510016+grisonb@users.noreply.github.com>
2026-09-21 09:15:38 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const BFG_SW_VERSION = '5.01';
-const CACHE_NAME = 'briefing-fdf-test-v5.01-supaip-relay-direct-r1';
+const BFG_SW_VERSION = '5.02';
+const CACHE_NAME = 'briefing-fdf-test-v5.02-supaip-relay-diag200-r1';
 
 const LOCAL_ASSETS = [
   './manifest.json',
